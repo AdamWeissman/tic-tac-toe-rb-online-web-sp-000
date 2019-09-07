f3c99c08f0428716da2b52d40e6284d7e82b015b
Adam Weissman <adam@comedytragedyepic.com>
2019-09-07 14:02:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -83,7 +83,7 @@ end
 #end
 
 def winner(board)
-  (board[won?(board)[0]] != nil) ? board[won?(board)[0]]) : nil 
+  (board[won?(board)[0]] != nil) ? return board[won?(board)[0]]) : return nil 
 end
 
 #def winner(board)  NAMEERROR
